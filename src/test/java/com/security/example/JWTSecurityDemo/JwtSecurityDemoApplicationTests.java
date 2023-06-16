6fb1f8b204bdc64969e4260aad4e85cccfc39507
sangeetha <raok.sangeetha@gmail.com>
2023-06-16 16:54:40 +0530

    first commit

---
+++ src/test/java/com/security/example/JWTSecurityDemo/JwtSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.example.JWTSecurityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
